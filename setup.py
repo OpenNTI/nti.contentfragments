657b64e2bfbf29b8be1c6afd2ff058a7b8f7b214
Jason Madden <jamadden@gmail.com>
2020-06-17 11:42:13 -0500

    Back to development: 1.4.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def _read(fname):
     with codecs.open(fname, encoding='utf-8') as f:
         return f.read()
 
-version = '1.4.0'
+version = '1.4.1.dev0'
 
 setup(
     name='nti.contentfragments',
