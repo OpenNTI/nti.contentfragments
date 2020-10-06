6b0033352d5a337fb5e36a70bdc3d4c2f7fefd94
Bobby Hagen <bobby.hagen@nextthought.com>
2020-10-06 18:21:13 -0500

    Provide conversion from rST to plain text.

--- src/nti/contentfragments/tests/test_rst.py
+++ src/nti/contentfragments/tests/test_rst.py
@@ -7,11 +7,17 @@ __docformat__ = "restructuredtext en"
 from hamcrest import assert_that
 from hamcrest import calling
 from hamcrest import raises
+from hamcrest import is_
+
+from nti.contentfragments.interfaces import IPlainTextContentFragment
+from nti.contentfragments.interfaces import IRstContentFragment
 
 from nti.contentfragments.rst import check_user_rst
 from nti.contentfragments.rst import RstParseError
 
 from nti.contentfragments.tests import ContentfragmentsLayerTest
+
+from nti.testing.matchers import verifiably_provides
 
 
 class TestRST(ContentfragmentsLayerTest):
@@ -24,3 +30,13 @@ class TestRST(ContentfragmentsLayerTest):
         content = "=====\nTitle\n=====\n\n.. sidebar:: sidebar title\n\nSome more content"
         assert_that(calling(check_user_rst).with_args(content),
                     raises(RstParseError, "Content block expected"))
+
+    def test_plaintext(self):
+        content = u"== ==\nH1 H2\n-- --\nc1 c2\n== ==\n\n"\
+                  u".. sidebar:: sidebar title\n\n   Some more content"
+        rst_content = IRstContentFragment(content)
+        assert_that(rst_content, verifiably_provides(IRstContentFragment))
+
+        plaintext = IPlainTextContentFragment(rst_content)
+        assert_that(plaintext, is_("\n\n\n\nH1\n\nH2\n\nc1\n\nc2\n\nsidebar title\n\nSome more content"))
+
